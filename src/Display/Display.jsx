071669ba9cc4e1b4e97755277b6cc6632215e5d7
Abubakar1010 <abubakar850772@gmail.com>
2024-03-12 05:46:28 +0600

    added styles and toggle functionality

--- src/Display/Display.jsx
+++ src/Display/Display.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import './Display.css'
 const Display = ({data}) => {
-    console.log(data);
+    
+    const [isVisited, setIsVisited] = useState(false)
 
-    const { name, capital,flags,population,area} = data
+    const toggleVisited = () => {
+
+        setIsVisited(!isVisited)
+    }
+
+    const { name, capital,flags,population,area, cca3} = data
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: isVisited? "rgb(222 112 149)" : 'rgb(208 133 61)'}}>
             <h2>Name: {name.common} </h2>
             <h2>Capital:{capital}</h2>
             <img src={flags.png} alt="" style={{ height: "200px", width: "300px"}} />
             <p>Population: {population} </p>
             <p>Area: {area} </p>
+            <p>Code: {cca3} </p>
+            <button onClick={toggleVisited} >{isVisited? "visited" : "Going"}</button>
+            <h2>{ isVisited? "Share your experience on this country" : "Go and explore this country"}</h2>
         </div>
     );
 };
